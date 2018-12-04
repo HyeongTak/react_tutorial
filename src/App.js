678a77ff5b813b5bb337584e4a1f2cb9fcbea4e7
전형탁 <andy395023@gmail.com>
2018-12-04 13:26:18 +0900

    함수형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Counter from './Counter';
+import MyName from './MyName';
 
 class App extends Component {
   render() {
     return (
-      <Counter />
+      <MyName />
     );
   }
 }
