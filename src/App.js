a7cf27223def5086b230844bea60ff8dc23ceeb9
전형탁 <andy395023@gmail.com>
2018-12-06 22:33:11 +0900

    배열

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
 
 class App extends Component {
+  id = 2;
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '최화랑',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '이준원',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id:this.id++ , ...data})
+    })
   }
   render() {
+    const { information } = this.state;
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
         />
+        {JSON.stringify(information)}
       </div>
     );
   }
